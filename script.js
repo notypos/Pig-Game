30d15e1bbf05b3cdbcf7085fd08611dbc11fd662
Ike Machover <126364189+notypos@users.noreply.github.com>
2024-02-16 08:30:08 -0500

    added full implementation of switch player

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
 // Function for score--1
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEL = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -38,9 +41,17 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    // When current--0 player 0 score will update. current--1 player 1 will update
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore; only changes player0 score
   } else {
     // Switch to next plyer
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // Switch has to go after you change the text
+    player0El.classList.toggle('player--active'); // css background with a transparency
+    player1El.classList.toggle('player--active');
   }
 });
 
